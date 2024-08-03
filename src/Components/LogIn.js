b523e5babb0f245b787909403320f16d5bbf8a02
Dikshant Singh <singhdikshant200@gmail.com>
2024-08-03 16:16:03 +0530

    after sign In/signUp adduser on redux store and navigate it to browse route

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -11,10 +11,12 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 const LogIn = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
 
   const name = useRef(null);
   const email = useRef(null);
@@ -55,6 +57,7 @@ const LogIn = () => {
           // Signed up
           const user = userCredential.user;
           console.log(user);
+          navigate("/browse");
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -72,6 +75,7 @@ const LogIn = () => {
           // Signed in
           const user = userCredential.user;
           console.log(user);
+          navigate("/browse");
         })
         .catch((error) => {
           const errorCode = error.code;
